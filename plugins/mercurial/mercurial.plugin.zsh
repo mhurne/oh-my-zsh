e290d068f36d965e404af738bbfb2ba3cf80d8b4
Matt Hurne <matt@thehurnes.com>
2016-11-10 13:15:48 -0500

    Disable use of hg status in mercurial plugin, which is too slow to be worthwhile for some repos

--- plugins/mercurial/mercurial.plugin.zsh
+++ plugins/mercurial/mercurial.plugin.zsh
@@ -34,26 +34,9 @@ function hg_prompt_info {
   if [ $(in_hg) ]; then
     _DISPLAY=$(hg_get_branch_name)
     echo "$ZSH_PROMPT_BASE_COLOR$ZSH_THEME_HG_PROMPT_PREFIX\
-$ZSH_THEME_REPO_NAME_COLOR$_DISPLAY$ZSH_PROMPT_BASE_COLOR$ZSH_PROMPT_BASE_COLOR$(hg_dirty)$ZSH_THEME_HG_PROMPT_SUFFIX$ZSH_PROMPT_BASE_COLOR"
+$ZSH_THEME_REPO_NAME_COLOR$_DISPLAY$ZSH_PROMPT_BASE_COLOR$ZSH_PROMPT_BASE_COLOR$ZSH_THEME_HG_PROMPT_CLEAN$ZSH_THEME_HG_PROMPT_SUFFIX$ZSH_PROMPT_BASE_COLOR"
     unset _DISPLAY
   fi
-}
-
-function hg_dirty_choose {
-  if [ $(in_hg) ]; then
-    hg status 2> /dev/null | command grep -Eq '^\s*[ACDIM!?L]'
-    if [ $pipestatus[-1] -eq 0 ]; then
-      # Grep exits with 0 when "One or more lines were selected", return "dirty".
-      echo $1
-    else
-      # Otherwise, no lines were found, or an error occurred. Return clean.
-      echo $2
-    fi
-  fi
-}
-
-function hg_dirty {
-  hg_dirty_choose $ZSH_THEME_HG_PROMPT_DIRTY $ZSH_THEME_HG_PROMPT_CLEAN
 }
 
 function hgic() {
